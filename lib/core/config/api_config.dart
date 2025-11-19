5870b7f9ae2d3f429b44a4027f30c09a519cc45a
kietvo5924 <kietcun100@gmail.com>
2025-11-19 21:07:10 +0700

    update

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -3,7 +3,7 @@ class ApiConfig {
   // 10.0.2.2 cho Android Emulator
   // localhost cho iOS simulator / web
   // IP thật của máy tính (ví dụ: 192.168.1.10) cho thiết bị vật lý
-  static const String baseUrl = 'http://192.168.1.6:8080/api';
+  static const String baseUrl = 'http://192.168.1.7:8080/api';
 
   // Endpoints cho Authentication
   static const String signInEndpoint = '/auth/signin';
